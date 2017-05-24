a6cdcded64d9fa258a0a3df09f223eaf0732df0a
miau <dejame@hola.com>
2017-05-24 14:36:40 +0200

    deleted stuff

--- hello.c
+++ hello.c
@@ -17,19 +17,6 @@ const char message[] =
 	"To generate this message, Docker took the following steps:\n"
 	" 1. The Docker client contacted the Docker daemon.\n"
 	" 2. The Docker daemon pulled the \"" DOCKER_IMAGE "\" image from the Docker Hub.\n"
-	" 3. The Docker daemon created a new container from that image which runs the\n"
-	"    executable that produces the output you are currently reading.\n"
-	" 4. The Docker daemon streamed that output to the Docker client, which sent it\n"
-	"    to your terminal.\n"
-	"\n"
-	"To try something more ambitious, you can run an Ubuntu container with:\n"
-	" $ docker run -it ubuntu bash\n"
-	"\n"
-	"Share images, automate workflows, and more with a free Docker ID:\n"
-	" https://cloud.docker.com/\n"
-	"\n"
-	"For more examples and ideas, visit:\n"
-	" https://docs.docker.com/engine/userguide/\n"
 	"\n";
 
 void _start() {
